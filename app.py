41a0978b1c70e49e91f8e4096289ae7f2285eea5
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-09-02 16:31:13 -0300

    test api

--- app.py
+++ app.py
@@ -1,39 +1,73 @@
-from flask import Flask, request
-import speech_recognition as sr 
+from flask import Flask, request, send_file, redirect, url_for, jsonify
+from flasgger import Swagger
+
+import speech_recognition as sr
 import moviepy.editor as mp
+import yt_dlp
 import time
+
 app = Flask(__name__)
+Swagger(app)
+
 
 @app.get("/")
 def home():
-    return "<p>Mostrar o swegger aqui</p>"
+    return redirect(url_for("flasgger.apidocs"))
 
-@app.post('/transcrever')
+
+@app.post("/transcrever")
 def transcribe():
+    """
+    Transcrever vídeo
+    ---
+    consumes:
+      - multipart/form-data
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+        description: Vídeo para realizar transcrição
+    responses:
+      200:
+        description: Transcrição
+        examples:
+          application/json: { "text": "Texto do vídeo" }
+    """
+
     file = request.files["file"]
-    
-    # path_file = f"uploads/{int(time.time())}-{file.filename}"
-    # path_file = f"uploads/{file.filename}"
-    path_file = f"uploads/test.mp4"
-    
-    print(path_file)
-    
+
+    path_file = f"uploads/{file.filename}"
+
     file.save(path_file)
-    
+
     video = mp.VideoFileClip(path_file)
 
     audio = video.audio
-    
-    audio.write_audiofile("uploads/test.wav")
-        
-    with audio as source:
-        recog = sr.Recognizer()
-        audio_data = recog.record(source)
-        
-        text = recog.recognize_google(audio_data, language="pt-BR")
-        
-        return {"text":text}
-    
 
-def download(url):
-    return 'download o video youtube'
+    audio.write_audiofile("uploads/temp.wav")
+
+    with sr.AudioFile("uploads/temp.wav") as source:
+
+        recog = sr.Recognizer()
+
+        audio_data = recog.record(source)
+
+        text = recog.recognize_google(audio_data, language="pt-BR")
+        return {"text": text}
+
+
+@app.get("/<yt_id>")
+def download(yt_id):
+
+    url = f'https://www.youtube.com/watch?v={yt_id}'
+
+    file_name = f"{int(time.time())}.mp4"
+
+    ydl_opts = {"format": "best", "outtmpl": file_name}
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return    send_file(file_name, as_attachment=True)
+    
--- test_api.py
+++ test_api.py
@@ -1,20 +1,37 @@
-import pytest 
+import pytest
 import requests
+
 # import tempfile
 
-api = 'http://localhost:5000'
+api = "http://localhost:5000"
+
 
 def test_get():
     res = requests.get(api)
-    
+
     assert res.status_code == 200
-    
-@pytest.mark.only 
+
+
 def test_post_transcribe():
-        
-    with open("uploads/olimpiadas.mp4", 'rb') as file:
+
+    with open("uploads/videoTeste.mp4", "rb") as file:
         files = {"file": file}
-        
-        res = requests.post(api+'/transcrever', files=files )
+
+        res = requests.post(api + "/transcrever", files=files)
+
+        print(res.json()["text"])
+
+        assert (
+            res.json()["text"]
+            == "rapazes vamos começar a contar uma linda história parece que vai ser uma aventura e tanto diário de bordo fotos músicas e Animação coloque trecho para retrospectiva rico parece que temos um lindo trabalho para fazer vamos fazer uma linda retrospectiva animada é capaz do recruta não sobreviver simbora"
+        )
+
+@pytest.mark.only
+def test_post_download():
+
+    res = requests.post(
+        api + "/download", json={"url": "https://www.youtube.com/shorts/Z7hzcwg0x8I"}
+    )
     
-        assert res.text == 'transcrever file'
+    
+    print(res)
