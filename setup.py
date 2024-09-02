c8f97195d00a08e3462c95e78bdfe812ea1a3dc8
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-09-02 17:51:23 -0300

    rename files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transcricao-python',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Lucas de Sousa',
+    author_email='seu_email@example.com'
+)
